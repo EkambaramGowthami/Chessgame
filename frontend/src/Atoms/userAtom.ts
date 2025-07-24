4c5c6fb3fe99e86af36f0ae74ea50f8c4a84be45
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-07-24 22:53:33 +0530

    Update userAtom.ts

--- frontend/src/Atoms/userAtom.ts
+++ frontend/src/Atoms/userAtom.ts
@@ -24,7 +24,7 @@ export const signinAtom = atom<signin_Type>({
         password:""
     }
 });
-export const authUserAtom = atom({
+export const authUserAtom = atom<user_Type>({
     key: "authUserAtom",
     default: null, 
   });
