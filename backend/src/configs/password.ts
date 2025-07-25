234cda1283531ee697511da61381a9d8f9185b25
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-07-25 10:53:54 +0530

    Update password.ts

--- backend/src/configs/password.ts
+++ backend/src/configs/password.ts
@@ -8,7 +8,8 @@ passport.use(
     new GoogleStratagy({
         clientID:process.env.GOOGLE_CLIENT_ID!,
         clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL: "https://chessgame-backend-3y0j.onrender.com/auth/google/callback",
+        callbackURL: "https://chessgame-backend-428c.onrender.com/auth/google/callback",
+        
     },
     async (accessToken,refreshToken,profile,done)=>{
         try{
