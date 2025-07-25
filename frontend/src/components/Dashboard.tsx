39edbbf930488cb2780cd310c2e64437979fe968
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-07-25 21:10:16 +0530

    Update Dashboard.tsx

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -67,6 +67,7 @@ export const Dashboard = () => {
                         <div className="text-gray-200 font-medium space-x-2 text-center  md:text-left">
                             <span className="italic text-2xl">Stand</span><span className="italic text-2xl" >up</span><span className="text-[#367c2b] text-3xl italic font-semibold">Eight</span>
                         </div>
+                        <div className="text-2xl text-white font-medium rounded-xl p-2 bg-green-500">Let's Play</div>
 
                     </div>
 
