83a8787d961e63a076e78dd21dd34eeb8fa55118
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-07-25 19:05:44 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -238,7 +238,7 @@ app.use("/auth",router);
       res.cookie("authToken", token, {
         httpOnly: true,
         sameSite: "none", 
-        secure: true,
+        secure: false,
         path:"/"
       });
       console.log("Cookies received:", req.cookies);
@@ -278,7 +278,7 @@ app.use("/auth",router);
       res.cookie("authToken", token, {
         httpOnly: true,
         sameSite: "none", 
-        secure: true,   
+        secure: false,   
       });
   
       res.status(200).json({
